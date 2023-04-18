d4df62c00694f65d9677d8c2e35f4d0f31f73284
masen <masen.crim@gmail.com>
2023-04-18 17:02:21 -0400

    Added class Frame and contructor Frame to establish a window for the game to be used on top of. Extends JFrame to esatlbish code-index for printed GUI window.

--- srt/src/Frame.java
+++ srt/src/Frame.java
@@ -1,2 +1,12 @@
-public class Frame {
+import javax.swing.JFrame;
+
+public class Frame extends JFrame {
+    //The frame class that extends JFrame to create a window for the game
+    public Frame() {
+        setTitle("Tic Tac Toe");
+        setSize(300, 300);
+        setLocationRelativeTo(null);
+        Panel panel = new Panel();
+        add(panel);
+    }
 }
